631ff2d669dd227ea9fcbe27e9055286a47dcb27
fishyfishfishfish <114358433+fishyfishfishfish@users.noreply.github.com>
2023-01-06 20:38:00 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,38 @@
-var SpeechRecognition=window.webkitSpeechRecognition;
-var recognition=new SpeechRecognition();
+https://teachablemachine.withgoogle.com/models/2wKu5MsFu/
+Webcam.set({
+width:350,
+height:300,
+image_format:'png',
+png_quality:90
+});
 
-function start(){
-    document.getElementById("textbox").innerHTML="";
-    recognition.start();
+camera=document.getElementById("camera");
+Webcam.attach('#camera');
+
+function takepicture(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="capture_image" src="'+data_uri+'">';
+    });
 }
 
-recognition.onresult=function(event){
-    console.log(event);
-    var content=event.results[0][0].transcript;
-    console.log(content);
-    document.getElementById("textbox").innerHTML=content;
+console.log('ml5 version',ml5.version);
+
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/2wKu5MsFu/model.json',modelLoaded);
+
+function modelLoaded(){
+    console.log("modelLoaded");
+}
+function identify(){
+    img=document.getElementById("capture_image");
+    classifier.classify(img,gotResult);
+}
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("name_of_object").innerHTML=results[0].label;
+        document.getElementById("accuracy").innerHTML=results[0].confidence.toFixed(3);
+    }
 }
